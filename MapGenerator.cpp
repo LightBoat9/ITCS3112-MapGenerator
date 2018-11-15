ce2beea127d09b9011762d46bf5fcb014c279f79
Luke <lsloop4@uncc.edu>
2018-11-14 19:32:00 -0500

    Init

---
+++ MapGenerator.cpp
@@ -0,0 +1,26 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    /* Example for SFML
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML", sf::Style::None);
+    sf::RectangleShape rectangle;
+    rectangle.setSize(sf::Vector2f(100, 50));
+    rectangle.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(rectangle);
+        window.display();
+    }
+    */
+    return 0;
+}
